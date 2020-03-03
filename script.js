c72c37b3b386ff8f8488ea0cfcb4e1c6416325b0
Janis Rancourt <janis.rancourt@gmail.com>
2020-03-03 13:47:56 -0500

    basic logic and placeholder info

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// alert("test");
+const dayOneSchedule = [
+{
+	time: "10:00AM",
+	event: "Placeholder Speedrun",
+	host: "Host's Name Here",
+	guest: "Super Cool Voice Actor"
+},
+{
+	time: "11:00AM",
+	event: "Placeholder Showcase",
+	host: "Cool Host Dude",
+	guest: "Mr. Person Human"
+}
+]
+
+
+const day1 = document.querySelector("#day1");
+
+
+
+for (let i = 0; i < dayOneSchedule.length; i++) {
+
+	let slot = dayOneSchedule[i];
+
+	let slotNode = document.createElement('div');
+	slotNode.classList.add('slot');
+
+	let timeNode = document.createElement('div');
+	timeNode.classList.add('time');
+	timeNode.textContent = slot.time;
+	slotNode.appendChild(timeNode);
+
+	day1.appendChild(slotNode);
+
+}
